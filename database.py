c61a99cdbfb89185b9c27fc4cd6af9e8104f097e
rabiaabid-tech <rabiaabid431@email.com>
2026-06-06 22:57:52 +0500

    fix: database

--- database.py
+++ database.py
@@ -7,7 +7,11 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+connect_args = {}
+if "aivencloud.com" in SQLALCHEMY_DATABASE_URL:
+    connect_args = {"ssl": {"ca": None}}
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
