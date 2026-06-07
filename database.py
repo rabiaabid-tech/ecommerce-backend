7536fb25d16ba1841073e35df4ad10691e73e501
rabiaabid-tech <rabiaabid431@email.com>
2026-06-07 19:42:54 +0500

    Final commit

--- database.py
+++ database.py
@@ -7,11 +7,19 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-connect_args = {}
-if "aivencloud.com" in SQLALCHEMY_DATABASE_URL:
-    connect_args = {"ssl": {"ca": None}}
+# 1. Dynamically connection arguments based on environment
+db_connect_args = {}
+if SQLALCHEMY_DATABASE_URL and "aivencloud.com" in SQLALCHEMY_DATABASE_URL:
+    db_connect_args = {"ssl": {"ca": None}}
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
+# 2. EnableConnection pool settings
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args=db_connect_args, 
+    pool_size=10,         
+    max_overflow=20,      
+    pool_recycle=1800     
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from database import engine
 import models
 from routers import products, auth
+from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
@@ -12,7 +13,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="E-commerce API", version="1.0.0")
 
-
+app.add_middleware(GZipMiddleware, minimum_size=1000)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -20,6 +21,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(products.router)
 app.include_router(auth.router)
